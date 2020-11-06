3903d1490297a376a06479dab345ebcd81909768
Daniil Potemkin <daniilpot@live.com>
2020-11-06 21:23:39 +0300

    add webapi project;

---
+++ MathEventWebApi/Models/Event.cs
@@ -0,0 +1,9 @@
+namespace  MathEventWepApi.Models
+{
+    public class MathEventWepApi
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string annotation { get; set; }
+    }
+}
