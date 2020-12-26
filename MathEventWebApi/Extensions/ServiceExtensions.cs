47087c052e25dbd08ed2561fd49fef973d706bff
Daniil Potemkin <daniilpot@live.com>
2020-12-26 14:54:16 +0300

    refactor Service Configuration;

---
+++ MathEventWebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,53 @@
+﻿using MathEventWebApi.Data;
+using MathEventWebApi.Data.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace MathEventApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureAuthentication(this IServiceCollection services)
+        {
+            // accepts any access token issued by identity server
+            services.AddAuthentication()
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://localhost:5001";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+        }
+
+        public static void ConfigureAuthorization(this IServiceCollection services)
+        {
+            // adds an authorization policy to make sure the token is for scope 'matheventapi'
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "matheventapi");
+                });
+            });
+        }
+
+        public static void ConfigureIndentity(this IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MathEventContext>()
+                .AddDefaultTokenProviders();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
+            });
+        }
+    }
+}
--- MathEventWebApi/Startup.cs
+++ MathEventWebApi/Startup.cs
@@ -1,17 +1,13 @@
 using System;
-using System.Security.Claims;
 using AutoMapper;
+using MathEventApi.Extensions;
 using MathEventWebApi.Data;
-using MathEventWebApi.Data.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Serialization;
 
 namespace MathEventWebApi
@@ -29,35 +25,11 @@ namespace MathEventWebApi
         {
             services.AddDbContext<MathEventContext>(options => options.UseSqlServer(
                 Configuration.GetConnectionString("DBConnection")));
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<MathEventContext>()
-                .AddDefaultTokenProviders();
 
+            services.ConfigureIndentity();
+            services.ConfigureAuthentication();
+            services.ConfigureAuthorization();
 
-            // accepts any access token issued by identity server
-            services.AddAuthentication()
-                .AddJwtBearer(options =>
-                {
-                    options.Authority = "https://localhost:5001";
-
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateAudience = false
-                    };
-                });
-
-            // adds an authorization policy to make sure the token is for scope 'matheventapi'
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("ApiScope", policy =>
-                {
-                    policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
-                    policy.RequireAuthenticatedUser();
-                    policy.RequireClaim("scope", "matheventapi");
-                });
-            });
-
-            services.Configure<IdentityOptions>(options => options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier);
             services.AddControllers().AddNewtonsoftJson(s => {
                 s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             });
