5454c2a6368ba07c12ae1323773e2c43b7b77270
Daniil Potemkin <daniilpot@live.com>
2021-03-29 20:02:09 +0300

    add client basic files;

---
+++ MathEvent.Client/mathevent.client/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = () => {
+    return {
+        entry: "./src/index.js",
+        output: {
+            path: path.join(__dirname, "/dist"),
+            filename: "bundle.js",
+            publicPath: "/"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"]
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        "sass-loader",
+                    ]
+                },
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.svg$/,
+                    use: [
+                        {
+                            loader: 'svg-url-loader',
+                            options: {
+                                limit: 10000,
+                            },
+                        },
+                    ],
+                },
+            ]
+        },
+        devServer: {
+            historyApiFallback: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            })
+        ],
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
+        }
+    }
+};
